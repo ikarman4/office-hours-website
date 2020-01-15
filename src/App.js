9187be13d5a326977455d16dfeb0888668003167
ikarman4 <52051882+ikarman4@users.noreply.github.com>
2020-01-14 19:12:32 -0500

    Styling

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const EPISODES = [
   {
     name: "DR. LIZ ARNOLD",
     description: "Learn more about Dr. Liz Arnold's industry experience and academic career in this episode of Office Hours.",
-    link: "https://google.com",
+    link: "https://drive.google.com/open?id=1Wtot-ZFAf1qys_8HgTCGWisydEYHtTh3",
     imageUrl: "https://ikarman4.github.io/LizArnold.jpg",
     longDescription: "Dr. Liz Arnold received her Ph.D. at the University of Texas at Austin. Throughout her career," +
       "she has done work for multiple notable pharmaceutical companies in New England such as Pfizer, Vertex, and Novartis." +
@@ -35,7 +35,7 @@ const EPISODES = [
   {
     name: "DR. KYLE GOBROGGE - A COMFORT IN NAIVETE, THE JOURNEY BEGINS",
     description: "Join us in hearing Zach's insight into graduate school along with his famous chili lime chicken recipe.",
-    link: "https://google.com",
+    link: "https://drive.google.com/open?id=1gwr3buSEdCGo_o295YzB3GTUH4BkZP-p",
     imageUrl: "https://ikarman4.github.io/KyleBioPic-e1563549753639.png",
     longDescription: "In this episode of Office Hours, Ian and Ankur set up shop in the office of their 'Principles of" +
       " Neuroscience' lab professor Dr. Kyle Gobrogge. An excellent teacher and an expert in the field of neurobiology, " +
@@ -43,6 +43,13 @@ const EPISODES = [
       "Cross Future Leaders Fellowship of the AAC&U. In this first episode, we cover various topics ranging from traits of " +
       "successful students in research to maintaining a work-life balance to the impact that policy can have on how research " +
       "is conducted."
+  },
+  {
+    name: "ARE WE DOING THIS RIGHT?",
+    description: "",
+    link: "https://drive.google.com/open?id=1Q9iV4LfvGbpAJNpoyJ5_fCJInEsheS98",
+    imageUrl: "https://github.com/ikarman4/ikarman4.github.io/blob/master/background.jpg",
+    longDescription: "Ian and Ankur attempt to put into words the creation of this podcast and what they have planned for it."
   }
 ];
 
