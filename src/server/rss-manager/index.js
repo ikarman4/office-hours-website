fc90a02a2e195bfddaa58c7731978f7c7a480ba2
ikarman4 <52051882+ikarman4@users.noreply.github.com>
2020-01-24 21:26:11 -0500

    rss update

--- src/server/rss-manager/index.js
+++ src/server/rss-manager/index.js
@@ -5,17 +5,17 @@ const getFeed = () => {
   /* lets create an rss feed */
   const feed = new Podcast({
     title: 'Office Hours',
-    description: 'Follow two curious kids as they navigate and explore big,' +
-    'seemingly intangible questions many people interested in research struggle' +
-    'with: what differentiates prolific researchers from thtose around them? Which' +
-     'topics or problems are worth spending a lifetime studying? What actionable' +
-     'steps can I take to illuminate my own interests? Join us in getting to know.' +
-      'As well as learning from researchers who are deeply passionate about their work.' +
-       'The leaders that are tasked with solving the ever-growing number of problems' +
+    description: 'Follow two curious kids as they navigate and explore big, ' +
+    'seemingly intangible questions many people interested in research struggle ' +
+    'with: what differentiates prolific researchers from those around them? Which ' +
+     'topics or problems are worth spending a lifetime studying? What actionable ' +
+     'steps can I take to illuminate my own interests? Join us in getting to know. ' +
+      'As well as learning from researchers who are deeply passionate about their work. ' +
+       'The leaders that are tasked with solving the ever-growing number of problems ' +
         'we face today.',
     feed_url: 'http://officehourspod.com/rss',
     site_url: 'http://officehourspod.com',
-    image_url: 'https://drive.google.com/file/d/1VFgFVQ6G52TwcN06LvA7GX3sRaHhZuog/preview',
+    image_url: 'https://office-hours-pod.s3.us-east-2.amazonaws.com/podcast-artwork.png',
     docs: 'http://officehourspod.com/rss/docs.html',
     author: 'Ian Karman',
     managingEditor: 'Ian Karman',
